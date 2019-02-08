fa5743efad0362dffd3f8c4c35689d3174adf9a5
Luke Pezet <lpezet@gmail.com>
2019-02-07 19:50:20 -0700

    New mysqls mod added with tests.

---
+++ tests/mysqls-test.js
@@ -0,0 +1,40 @@
+const TestedClass = require('../lib/mysqls');
+const load_file = require('./load_file');
+
+describe('mysqls',function(){
+	
+	before(function(done) {
+		done();
+	});
+	
+	after(function(done) {
+		done();
+	});
+
+	it('basic',function(done){
+    	
+    	var ExecutorClass = function() {};
+    	ExecutorClass.prototype.exec = function( pCmd, pCmdOpts, pCallback ) {
+    		switch ( pCmdOpts.context ) {
+    			case "/a/b/c.txt":
+    				assert.include( pCmd, "--execute='SELECT * FROM test'");
+    				break;
+    		}
+    		pCallback( null, "", "");
+    	}
+    	
+    	var oExecutor = new ExecutorClass();
+    	var oTested = new TestedClass( oExecutor );
+    	
+		var oConfig = load_file( './mysqls/basic.yml');
+		
+		oTested.handle( 'root', oConfig['root'], oExecutor ).then(function() {
+			done();
+		}, function( pError ) {
+			console.log( pError );
+			done( pError );
+		})
+		
+    	
+	});
+});

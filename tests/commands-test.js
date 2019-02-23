eb33a564d11e31c1647c779eb58a3b2ef4ee2057
Luke Pezet <lpezet@gmail.com>
2019-02-22 21:30:45 -0700

    test added for new commands mod feature.

--- tests/commands-test.js
+++ tests/commands-test.js
@@ -18,6 +18,95 @@ describe('commands',function(){
 	after(function(done) {
 		done();
 	});
+	
+	it('result_as_normal', function(done) {
+		var ExecutorClass = function() {};
+    	ExecutorClass.prototype.exec = function( pCmd, pCmdOpts, pCallback ) {
+    		pCallback(null, '[ "Toto", "Tutu" ]');
+    	};
+    	var oExecutor = new ExecutorClass();
+    	var oTested = new TestedClass();
+    	
+		var oTemplate = {
+				root: {
+					"001_json": {
+						command: "dontmatter",
+						result_as_json: false
+					}
+				}
+		}
+		
+		oTested.handle( 'root', oTemplate['root'], oExecutor ).then(function( pData ) {
+			//console.dir( pData );
+			assert.exists( pData['commands'] );
+			assert.exists( pData[ 'commands' ][ '001_json' ] );
+			assert.isNotArray( pData[ 'commands' ][ '001_json' ][ 'result' ] );	
+			done();
+		}, function( pError ) {
+			console.log( pError );
+			done( pError );
+		})
+	});
+	
+	it('result_as_json', function(done) {
+		var ExecutorClass = function() {};
+    	ExecutorClass.prototype.exec = function( pCmd, pCmdOpts, pCallback ) {
+    		pCallback(null, '[ "Toto", "Tutu" ]');
+    	};
+    	var oExecutor = new ExecutorClass();
+    	var oTested = new TestedClass();
+    	
+		var oTemplate = {
+				root: {
+					"001_json": {
+						command: "dontmatter",
+						result_as_json: true
+					}
+				}
+		}
+		
+		oTested.handle( 'root', oTemplate['root'], oExecutor ).then(function( pData ) {
+			//console.dir( pData );
+			assert.exists( pData['commands'] );
+			assert.exists( pData[ 'commands' ][ '001_json' ] );
+			assert.isArray( pData[ 'commands' ][ '001_json' ][ 'result' ] );	
+			done();
+		}, function( pError ) {
+			console.log( pError );
+			done( pError );
+		})
+	});
+	
+	it('invalid_json_result_as_json', function(done) {
+		var ExecutorClass = function() {};
+    	ExecutorClass.prototype.exec = function( pCmd, pCmdOpts, pCallback ) {
+    		// Evil: single quotes are not valid in JSON...
+    		pCallback(null, "[ 'Toto', 'Tutu' ]");
+    	};
+    	var oExecutor = new ExecutorClass();
+    	var oTested = new TestedClass();
+    	
+		var oTemplate = {
+				root: {
+					"001_json": {
+						command: "dontmatter",
+						result_as_json: true
+					}
+				}
+		}
+		
+		oTested.handle( 'root', oTemplate['root'], oExecutor ).then(function( pData ) {
+			//console.dir( pData );
+			assert.exists( pData['commands'] );
+			assert.exists( pData[ 'commands' ][ '001_json' ] );
+			assert.isNotArray( pData[ 'commands' ][ '001_json' ][ 'result' ] );	
+			assert.equal( pData[ 'commands' ][ '001_json' ][ 'result' ], "[ 'Toto', 'Tutu' ]" );	
+			done();
+		}, function( pError ) {
+			console.log( pError );
+			done( pError );
+		})
+	});
 
 	it('basic',function(done){
     	

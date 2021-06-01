e591ee59af6f3337eeebc1e9bcd28c890ed95432
lpezet <lpezet@gmail.com>
2021-05-31 23:56:37 -0400

    Deleted version based on previous architecture.

--- lib/index.ts
+++ lib/index.ts
@@ -1,44 +1,44 @@
-export { default as ETL, IETL, ModCallback } from "./etl";
-export { default as CommandsMod, Data as CommandsData } from "./commands";
-export { default as Context } from "./context";
+export { default as ETL, IETL, ModCallback } from "./rearch/etl";
+export { default as CommandsMod, Data as CommandsData } from "./rearch/mods/commands";
+export { default as Context } from "./rearch/context";
 export {
   Callback as ExecutorCallback,
   Local,
   Remote,
   Executor
-} from "./executors";
+} from "./rearch/executors";
 export {
   default as FilesMod,
   Permissions as FilesPermissions,
   Data as FilesData
-} from "./files";
+} from "./rearch/mods/files";
 export {
   default as HPCCDespraysMod,
   Data as HPCCDespraysData
-} from "./hpcc-desprays";
-export { default as HPCCECLsMod, Data as HPCCECLsData } from "./hpcc-ecls";
+} from "./rearch/mods/hpcc-desprays";
+export { default as HPCCECLsMod, Data as HPCCECLsData } from "./rearch/mods/hpcc-ecls";
 export {
   default as HPCCSpraysMod,
   Data as HPCCSpraysData
-} from "./hpcc-sprays";
+} from "./rearch/mods/hpcc-sprays";
 export {
   default as ImageChartsMod,
   Data as ImageChartsData
-} from "./image-charts";
+} from "./rearch/mods/image-charts";
 export {
   default as InteractivesMod,
   Data as InteractivesData
-} from "./interactives";
+} from "./rearch/mods/interactives";
 export {
   Logger,
   Configuration as LoggerConfiguration,
   createLogger,
   configureLogger
-} from "./logger";
-export { default as Mod } from "./mod";
+} from "./rearch/logger";
+export { default as Mod } from "./rearch/mod";
 export {
   default as MySQLImportsMod,
   Data as MySQLImportsData
-} from "./mysqlimports";
-export { default as MySQLsMod, Data as MySQLsData } from "./mysqls";
-export * as Promises from "./promises";
+} from "./rearch/mods/mysqlimports";
+export { default as MySQLsMod, Data as MySQLsData } from "./rearch/mods/mysqls";
+export * as Promises from "./rearch/promises";
